8502a2cd5c6491b0751af0fb73da51cb1b0abbd1
reggi49 <reggimuhamad@gmail.com>
2021-11-12 15:44:35 +0700

    clone ugc deploy

---
+++ build/precache-manifest.d00e7280be4a9d8f41e2876b83afb845.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f142020c32957b5e2369c2972a50eb3",
+    "url": "/reggi49.github.io/index.html"
+  },
+  {
+    "revision": "5798ef47750152d6500f",
+    "url": "/reggi49.github.io/static/css/2.3cc6f9d3.chunk.css"
+  },
+  {
+    "revision": "11c85554e488a5e677cf",
+    "url": "/reggi49.github.io/static/css/main.5138d059.chunk.css"
+  },
+  {
+    "revision": "5798ef47750152d6500f",
+    "url": "/reggi49.github.io/static/js/2.888206d2.chunk.js"
+  },
+  {
+    "revision": "2e5a342cd4e4f17908dd1dbde2707c42",
+    "url": "/reggi49.github.io/static/js/2.888206d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c85554e488a5e677cf",
+    "url": "/reggi49.github.io/static/js/main.db8c8d3a.chunk.js"
+  },
+  {
+    "revision": "772efe5590ad79986fbf3a1db778be8b",
+    "url": "/reggi49.github.io/static/js/main.db8c8d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c97bd82b8f8ffd8aaa5",
+    "url": "/reggi49.github.io/static/js/runtime-main.249c394f.js"
+  },
+  {
+    "revision": "47e30d2caaa972cdf7b4b3005881528d",
+    "url": "/reggi49.github.io/static/media/Antenna-Regular.47e30d2c.otf"
+  },
+  {
+    "revision": "0609428a5dd9ccaeed9d3a661e97f112",
+    "url": "/reggi49.github.io/static/media/MBtech-Logo.0609428a.png"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/reggi49.github.io/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/reggi49.github.io/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/reggi49.github.io/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/reggi49.github.io/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "63da0e0ccd81688b90121b9dda9b7e6e",
+    "url": "/reggi49.github.io/static/media/Powered-By-Mbtech.63da0e0c.png"
+  },
+  {
+    "revision": "41ed1617fca3d945e1856a7011b7ebe0",
+    "url": "/reggi49.github.io/static/media/TrajanPro-Regular.41ed1617.ttf"
+  },
+  {
+    "revision": "223bc6610a336053e356251eec79c16e",
+    "url": "/reggi49.github.io/static/media/clean-method.223bc661.png"
+  },
+  {
+    "revision": "de746dced91d09a3746420898b807373",
+    "url": "/reggi49.github.io/static/media/color-collection.de746dce.png"
+  },
+  {
+    "revision": "d0f2810c8bc4c26aa11cd0831c1a8768",
+    "url": "/reggi49.github.io/static/media/colour-icon-500.d0f2810c.png"
+  },
+  {
+    "revision": "487e72192fe5aaec31b44c98e0d0d2a5",
+    "url": "/reggi49.github.io/static/media/detail_produk.487e7219.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/reggi49.github.io/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/reggi49.github.io/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/reggi49.github.io/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/reggi49.github.io/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/reggi49.github.io/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f82003411270ef4ae8521c4fb401cf4b",
+    "url": "/reggi49.github.io/static/media/hand-qrcode.f8200341.png"
+  },
+  {
+    "revision": "33f378f76213cb7cffc1f6c3c71d0e7c",
+    "url": "/reggi49.github.io/static/media/handphone-askmbtech.33f378f7.png"
+  },
+  {
+    "revision": "d4d195e4186c652496a60149f6558e01",
+    "url": "/reggi49.github.io/static/media/handphone-locator.d4d195e4.png"
+  },
+  {
+    "revision": "e38ff0ad353d1074c44f444080f8bf3b",
+    "url": "/reggi49.github.io/static/media/handphone_banner.e38ff0ad.png"
+  },
+  {
+    "revision": "2e90eabee009d8b75d5be6697d559e13",
+    "url": "/reggi49.github.io/static/media/icon-consultation.2e90eabe.png"
+  },
+  {
+    "revision": "acd3e78379c570e3f996ed078dd9d867",
+    "url": "/reggi49.github.io/static/media/icon-scan-qr.acd3e783.png"
+  },
+  {
+    "revision": "8fd0dfd3a54c39ff1ea6fb823aecf28c",
+    "url": "/reggi49.github.io/static/media/location-icon.8fd0dfd3.png"
+  },
+  {
+    "revision": "5ac073e1405ba5482d5d4f1b9ca360e9",
+    "url": "/reggi49.github.io/static/media/logo_mbtech.5ac073e1.png"
+  },
+  {
+    "revision": "cdbe7eb84706a7e30ef36767e21e26f2",
+    "url": "/reggi49.github.io/static/media/produk-icon.cdbe7eb8.png"
+  },
+  {
+    "revision": "dabd02e8db920139e2da4198407de8bd",
+    "url": "/reggi49.github.io/static/media/profile-seatmaker.dabd02e8.png"
+  },
+  {
+    "revision": "a7932a69fdd5efff71f98abfe1622e06",
+    "url": "/reggi49.github.io/static/media/seatmaker-locator.a7932a69.png"
+  }
+]);

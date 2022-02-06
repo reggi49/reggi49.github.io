1ba46fa7d7bd3a9ddc3ab6f29b54d480f08ace21
regi <regi>
2022-02-06 19:20:17 +0700

    update detail article

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ const Home = () => {
   const [portfolio, setPortfolio] = useState([]);
   const [modal, setModal] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [article, setArticle] = useState(false);
 
+  const hostUrl = window.location.host;
   const client = createClient({
     space: "38v5evtkgftl",
     accessToken: "kDgiFMQFZAObsWasOixfK8UYG5Ij7E8wmj-UE88zl8Y",
@@ -105,7 +105,6 @@ const Home = () => {
         })
         .then((portfolio) => {
           setPortfolio(portfolio.items[0]);
-          setArticle(false)
           setLoading(false);
           console.log(portfolio);
         })
@@ -138,11 +137,9 @@ const Home = () => {
               <div class="col-md-10 col-lg-8 col-xl-7">
                 <div class="post-heading">
                   <h1>{portfolio == "" ? "" : portfolio.fields.title}</h1>
-                  <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+                  <h2 class="subheading">{portfolio == "" ? "" : portfolio.fields.description}</h2>
                   <span class="meta">
-                    Posted by
-                    <a href="#!">Start Bootstrap</a>
-                    on August 24, 2021
+                    Posted by {portfolio == "" ? "" : portfolio.fields.author.fields.name} 
                   </span>
                 </div>
               </div>
@@ -152,34 +149,12 @@ const Home = () => {
         <article class="mb-4">
           <div class="container px-4 px-lg-5">
           <ul class="breadcrumb">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Fruit</a></li>
-            <li class="active">Pears</li>
+            <li><a href={hostUrl}>Home</a></li>
+            <li class="active">{portfolio == "" ? "" : portfolio.fields.title}</li>
           </ul>
-            <div class="drow gx-4 gx-lg-5 justify-content-center">
-              <div class="col-md-10 col-lg-8 col-xl-7">
-                <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
-                <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
-                <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>
-                <p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
-                <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>
-                <h2 class="section-heading">The Final Frontier</h2>
-                <p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
-                <p>There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the generations. And no matter how much progress one makes, there is always the thrill of just beginning.</p>
-                <blockquote class="blockquote">The dreams of yesterday are the hopes of today and the reality of tomorrow. Science has not yet mastered prophecy. We predict too much for the next year and yet far too little for the next ten.</blockquote>
-                <p>Spaceflights cannot be stopped. This is not the work of any one man or even a group of men. It is a historical process which mankind is carrying out in accordance with the natural laws of human development.</p>
-                <h2 class="section-heading">Reaching for the Stars</h2>
-                <p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
-                <a href="#!"><img class="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a>
-                <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
-                <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
-                <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
-                <p>
-                  Placeholder text by
-                  <a href="http://spaceipsum.com/">Space Ipsum</a>
-                  &middot; Images by
-                  <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>
-                </p>
+            <div class="drow gx-4 gx-lg-5 justify-content-center pb-3rem">
+              <div class="col-md-10 col-lg-8 col-xl-7 ">
+                {portfolio == "" ? "" : portfolio.fields.body}
               </div>
             </div>
           </div>
@@ -230,7 +205,7 @@ const Home = () => {
               <p className="text_body_portfolio text-center">
                 {loading == true
                   ? "Getting Description ..."
-                  : portfolio.fields.body}
+                  : portfolio.fields.description}
               </p>
               <Link
                 className="posts__post"
@@ -408,13 +383,13 @@ const Home = () => {
             </a>
           </li> */}
               <li>
-                <a href="#portfolio">Portfolio</a>
+                <a href={hostUrl + "/#portfolio"}>Portfolio</a>
               </li>
               <li>
-                <a href="#services">Services</a>
+                <a href={hostUrl + "/#portfolio"}>Services</a>
               </li>
               <li>
-                <a href="#contact">Contact</a>
+                <a href={hostUrl + "/#contact"}>Contact</a>
               </li>
               <li>
                 <a href="https://reggi49.medium.com/" target="_blank">
@@ -573,7 +548,7 @@ const Home = () => {
                               <div key={key} className="col-md-4">
                                 <div className="panel" style={{ border: "none" }}>
                                   <div className="d-flex flex-row mb-3">
-                                    <a onClick={() => getPortfolios(item.sys.id)}>
+                                    <a onClick={() => getPortfolios(item.fields.slug)}>
                                       <img
                                         className="image_portfolio"
                                         src={item.fields.heroImage.fields.file.url}
